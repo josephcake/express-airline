d61ecd351bdce1f576a48f1a169261060ce44469
Joseph Gao <joseph8p@gmail.com>
2021-02-17 15:48:06 -0500

    input styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import styled from 'styled-components'
 
+const Input = styled.input`
+  font-family: sans-serif;
+  font-size: 20px;
+  border:none;
+  border-radius:5px;
+  outline:none;  
+`
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <Input/>
+      </div>
     </div>
   );
 }
