4dd46bb3abc4194cfdd985a4a998848032ac55ab
Joseph Gao <joseph8p@gmail.com>
2021-02-18 15:47:57 -0500

    nav

--- src/navigation/Nav.js
+++ src/navigation/Nav.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import {NavItem} from './styled/NavItem'
+
+const Navbar = styled.div`
+  display:flex;
+  justify-content:space-evenly;
+  align-items:center;
+  width:100%;
+  height:50px;
+  padding:5px;
+`
 
 const Nav = () =>{
   return (
-    <div>
-
-    </div>
+    <Navbar>
+      <NavItem>Nav-item</NavItem>
+      <NavItem>Nav-item</NavItem>
+      <NavItem>Nav-item</NavItem>
+    </Navbar>
   )
 }
 
